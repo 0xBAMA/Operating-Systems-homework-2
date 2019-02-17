8638d48afaa7ac163bc99c856caad2cd5fe1cba6
0xBAMA <jon.baker1222@gmail.com>
2019-02-17 17:19:47 -0500

    making serious progress

--- pp_ps.c
+++ pp_ps.c
@@ -26,10 +26,16 @@ struct entry{
 struct entry entries[500];
 u_int num_entries = 0;
 
+float uptime;
+
 void readdirs();
 
 
 int main(int argc, char const *argv[]) {
+
+  FILE *fp = fopen("/proc/uptime","r");
+
+  fscanf(fp,"%f",&uptime);
 
   readdirs();
 
@@ -40,6 +46,16 @@ int main(int argc, char const *argv[]) {
 void readdirs()
 {
   DIR *dp;
+  FILE *fp;
+
+  char data_string[1000];
+  char *token;
+
+  long utime;
+  long stime;
+
+  void *junk;
+
 
   struct dirent *ep;
 
@@ -59,7 +75,7 @@ void readdirs()
       {
         printf("directory name: %s \n", ep->d_name);
 
-        /*do stuff with the directory*/
+        /*assemble a string to represent the filename of this pid's stat file*/
 
         sprintf(p,"%d",pid);
 
@@ -71,7 +87,66 @@ void readdirs()
 
         strcat(beg,end);
 
-        printf("out: %s\n",beg);
+        /*beg holds the filename of /proc/<pid>/stat */
+
+        fp = fopen(beg,"r");
+
+        /*get the data from the file*/
+
+        fgets(data_string,1000,fp);
+
+        /*get the pid - first entry*/
+
+        token = strtok(data_string, " ");
+
+        entries[num_entries].pid = atoi(token);
+
+        /*get the command - second entry*/
+
+        token = strtok(NULL,")");
+
+        sprintf(token,"%s", token+1); /*dump the leading parenthesis*/
+
+        entries[num_entries].command = token;
+
+        /*get the state - third entry*/
+
+        token = strtok(NULL," ");
+
+        entries[num_entries].state = token;
+
+        /*get rid of junk values*/
+
+        token = strtok(NULL," ");/* fourth entry     - ppid*/
+        token = strtok(NULL," ");/* fifth entry      - pgrp*/
+        token = strtok(NULL," ");/* sixth entry      - session*/
+        token = strtok(NULL," ");/* seventh entry    - tty_nr*/
+        token = strtok(NULL," ");/* eigth entry      - tpgid*/
+        token = strtok(NULL," ");/* ninth entry      - flags*/
+        token = strtok(NULL," ");/* tenth entry      - minflt*/
+        token = strtok(NULL," ");/* eleventh entry   - cminflt*/
+        token = strtok(NULL," ");/* twelfth entry    - majflt*/
+        token = strtok(NULL," ");/* thirteenth entry - cmajflt*/
+
+        /*get the program's utime - fourteenth entry*/
+
+        token = strtok(NULL," ");
+        utime = atol(token);
+
+        /*get the program's stime - fifteenth entry*/
+
+        token = strtok(NULL," ");
+        stime = atol(token);
+
+        /*****compute the time, for %CPU******/
+
+
+
+        token = strtok(NULL," ");
+
+
+        num_entries++;
+        fclose(fp);
       }
     }
     closedir(dp);
